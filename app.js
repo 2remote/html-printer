b048426bd03137d055b433486f601461b842a47f
Hu Songtao <idisblueflash@gmail.com>
2016-04-12 22:07:43 +0800

    fetch url from api and show as article

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 var express = require('express');
+var request = require('request');
+var async = require('async');
 
 var app = express();
 app.use('/', express.static(__dirname + '/www'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var data = {Title: 'Not found'};
+
 app.get('/api/show/:id', function(req, res){
-  res.render('article', { title: req.params.id });
+  async.waterfall([fetchJson], function(err, result){
+    result = eval('(' + result + ')');
+    res.render('article', result);
+  });
 });
 
 var port = 6000;
 app.listen(port);
 console.log('Server running at http://127.0.0.1:%s.', port);
 
+function renderBack(data, res, callback) {
+  res.render('article', data);
+}
+
+function fetchJson(callback) {
+  request('http://api.aiyaopai.com/\?api\=Article.Get\&Id\=3\&Fields\=Title', 
+    function(err, res, body){
+      if(!err && res.statusCode == 200){
+        callback(null, body);
+      }
+    }
+  );
+}
