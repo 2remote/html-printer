662ff965776f3a43784b162e298ba523bb5c9916
Hu Songtao <idisblueflash@gmail.com>
2016-04-12 23:20:06 +0800

    save title into html file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var request = require('request');
 var async = require('async');
+var ejs = require('ejs');
+var fs = require('fs');
 
 var app = express();
 app.use('/', express.static(__dirname + '/www'));
@@ -9,34 +11,67 @@ app.set('view engine', 'ejs');
 
 var NotFound = {Title: 'Not found'};
 
+var template = '';
+loadEjsView('views/article.ejs', function(err, data){
+  template = data;
+});
+
 app.get('/api/show/:id', function(req, res){
   var article_id = req.params.id;
+  var file = 'www/' + article_id + '.html';
+
   async.waterfall([
-      async.apply(fetchJson, article_id)
+      async.apply(fetchJson, article_id),
+      async.apply(renderToFile, template),
+      async.apply(saveFile, file)
     ], 
-  function(err, result){
-    result = eval('(' + result + ')');
-    if (result.Success){
-      res.render('article', result);
-    }else{
-      res.render('article', NotFound);
+    function(err, result){
+      console.log(result);
+      res.json({result: result});
     }
-  });
+  );
 });
 
 var port = 6000;
 app.listen(port);
 console.log('Server running at http://127.0.0.1:%s.', port);
 
-function fetchJson(id, callback) {
-  request('http://api.aiyaopai.com/\?api\=Article.Get\&Id\='
+function fetchJson(id, callback) { // (id -- body)
+  request('http://dev.api.aiyaopai.com/\?api\=Article.Get\&Id\='
     + id
     + '\&Fields\=Title', 
     function(err, res, body){
       if(!err && res.statusCode == 200){
         console.log(body);
-        callback(null, body);
+        result = eval('(' + body + ')');
+        callback(null, result);
       }
     }
   );
 }
+
+function loadEjsView(file, callback) { // (file -- ejsFileString)
+  fs.exists(file, function(exists) {
+    if(exists) {
+      fs.readFile(file, 'utf8', function(err, data) {
+        if (err) throw err;
+        var ejsFileString = data.toString();
+        callback(null, ejsFileString);
+      })
+    }
+  })
+}
+
+function renderToFile(templateStr, data, callback){ // (templateStr, data -- html) 
+  var html = ejs.render(templateStr, data);
+  callback(null, html);
+}
+
+function saveFile(file, string, callback) { // (file, string -- )
+  fs.writeFile(file, string, function(err){
+    if(err) throw err;
+    console.log('The %s was saved!', file);
+  });
+  callback(null, {Success: true});
+}
+
