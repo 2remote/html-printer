e1bdd57986f0504101873d72998418437cb22d16
Hu Songtao <idisblueflash@gmail.com>
2016-04-12 21:09:24 +0800

    add ejs template and router

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 
 var app = express();
 app.use('/', express.static(__dirname + '/www'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/api/show/:id', function(req, res){
+  res.render('article', { title: req.params.id });
+});
 
 var port = 6000;
 app.listen(port);
