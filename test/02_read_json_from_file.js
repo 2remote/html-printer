6955e842e8de80b66af247221455412b38358464
Hu Songtao <idisblueflash@gmail.com>
2016-04-16 10:37:32 +0800

    fix: test json file location

--- test/02_read_json_from_file.js
+++ test/02_read_json_from_file.js
@@ -3,8 +3,8 @@ var common = require('../lib/common');
 
 test('read demo.json into array', function(t) {
   t.plan(2);
-  var current_path = common.getFilePath('./www/files.json');
+  var current_path = common.getFilePath('./test/files.json');
   t.equal(true, current_path.indexOf('files.json') >= 0);
-  var data = common.readJSON('./www/files.json');
+  var data = common.readJSON('./test/files.json');
   t.equal(3, data.length);
 });
